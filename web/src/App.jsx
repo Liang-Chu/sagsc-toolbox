00eeaa89a5dd94e2259eaa9707b14f04f3cadd82
liang chu <chux2890@mylaurier.ca>
2024-11-10 23:22:18 -0500

    bug fix

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { relaunch } from '@tauri-apps/plugin-process';
 import MainLayout from "./layouts/MainLayout";
 import ExcelTools from "./pages/ExcelTools";
 import { navOptions } from "./assets/Links";
@@ -9,21 +10,46 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [isUpdating, setIsUpdating] = useState(false) // Track Update state
   useEffect(() => {
     // Check for updates when the app starts
     const checkForUpdates = async () => {
       try {
-        const update = await check();
-        if (update) {
-          console.log(`Found update: ${update.version}`);
-          // You can display a notification or prompt the user to update
+        const updateInfo = await check() // Check for updates
+        console.log(`Update: ${JSON.stringify(updateInfo)}`)
+  
+        if (updateInfo && updateInfo.version !== updateInfo.currentVersion) {
+          let downloaded = 0
+          let contentLength = 0
+          await updateInfo.downloadAndInstall((event) => {
+            switch (event.event) {
+              case "Started":
+                contentLength = event.data.contentLength
+                console.log(
+                  `started downloading ${event.data.contentLength} bytes`
+                )
+                break
+              case "Progress":
+                downloaded += event.data.chunkLength
+                console.log(`downloaded ${downloaded} from ${contentLength}`)
+                break
+              case "Finished":
+                console.log("download finished")
+                break
+              default:
+                break
+            }
+          })
+          console.log("update installed")
+          await relaunch()
         } else {
-          console.log("No updates available");
+          console.log("No updates available")
         }
       } catch (error) {
-        console.error("Error checking for updates:", error);
+        console.error("Error checking for updates:", error)
+        setIsUpdating(false) // Reset loading state if an error occurs
       }
-    };
+    }
 
     checkForUpdates();
   }, []);
--- web/src/layouts/MainLayout.jsx
+++ web/src/layouts/MainLayout.jsx
@@ -1,35 +1,54 @@
-import React, { useState } from "react";
-import { check, Update } from '@tauri-apps/plugin-updater'; // Import necessary functions
-import NavBar from "../components/Navbar";
-import packageJson from "../../package.json"; // Import package.json to get the version
+import React, { useState } from "react"
+import { check } from "@tauri-apps/plugin-updater" // Import necessary functions
+import { relaunch } from '@tauri-apps/plugin-process';
+import NavBar from "../components/Navbar"
+import packageJson from "../../package.json" // Import package.json to get the version
 
 const MainLayout = ({ children }) => {
-  const [isSidebarVisible, setSidebarVisible] = useState(false);
-  const [isUpdating, setIsUpdating] = useState(false); // Track update state
+  const [isSidebarVisible, setSidebarVisible] = useState(false)
+  const [isUpdating, setIsUpdating] = useState(false) // Track Update state
 
   const toggleSidebar = () => {
-    setSidebarVisible(!isSidebarVisible);
-  };
+    setSidebarVisible(!isSidebarVisible)
+  }
 
   const checkForUpdates = async () => {
     try {
-      const update = await check();
-      console.log(`update: ${update}`);
-      if (update) {
-        console.log(`Found update: ${update.version}`);
-        // Start the download and installation process
-        setIsUpdating(true); // Show loading state
-        await Update();
-        setIsUpdating(false); // Reset loading state after installation
-        console.log("Update installed successfully.");
+      const updateInfo = await check() // Check for updates
+      console.log(`Update: ${JSON.stringify(updateInfo)}`)
+
+      if (updateInfo && updateInfo.version !== updateInfo.currentVersion) {
+        let downloaded = 0
+        let contentLength = 0
+        await updateInfo.downloadAndInstall((event) => {
+          switch (event.event) {
+            case "Started":
+              contentLength = event.data.contentLength
+              console.log(
+                `started downloading ${event.data.contentLength} bytes`
+              )
+              break
+            case "Progress":
+              downloaded += event.data.chunkLength
+              console.log(`downloaded ${downloaded} from ${contentLength}`)
+              break
+            case "Finished":
+              console.log("download finished")
+              break
+            default:
+              break
+          }
+        })
+        console.log("update installed")
+        await relaunch()
       } else {
-        console.log("No updates available");
+        console.log("No updates available")
       }
     } catch (error) {
-      console.error("Error checking for updates:", error);
-      setIsUpdating(false); // Reset loading state if an error occurs
+      console.error("Error checking for updates:", error)
+      setIsUpdating(false) // Reset loading state if an error occurs
     }
-  };
+  }
 
   return (
     <div className="App">
@@ -40,26 +59,32 @@ const MainLayout = ({ children }) => {
       >
         {isSidebarVisible ? "<<" : ">>"}
       </button>
-      <main className="main-container">
-        {children}
-      </main>
+      <main className="main-container">{children}</main>
       {/* Display version in the top right */}
-      <div 
-        className="version-info" 
-        style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }}
+      <div
+        className="version-info"
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          cursor: "pointer",
+        }}
         onClick={checkForUpdates}
       >
-        <p>Version: {packageJson.version}</p>
+        <p>Versiont : {packageJson.version}</p>
       </div>
 
       {/* Show a loading message while updating */}
       {isUpdating && (
-        <div className="updating-info" style={{ position: 'absolute', top: '50px', right: '10px' }}>
-          <p>Downloading and installing update...</p>
+        <div
+          className="updating-info"
+          style={{ position: "absolute", top: "50px", right: "10px" }}
+        >
+          <p>Downloading and installing Update...</p>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default MainLayout;
+export default MainLayout
